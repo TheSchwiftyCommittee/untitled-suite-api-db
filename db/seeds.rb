7e945999af35a3dfadaadcfcf1169ecc6ce0001c
NJBLAGA <nathanblaga90@gmail.com>
2021-07-27 20:11:08 +1000

    add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+list_item1 = List.create(title: "Daily workout", description: "It is Leg day", priority: "Important",  completed: false)
+list_item2 = List.create(title: "Clean Car", description: "Its been weeks", priority: "Sem-Important",  completed: false)
+list_item3 = List.create(title: "Cook dinner", description: "Kids will be home soon", priority: "Important",  completed: false)
