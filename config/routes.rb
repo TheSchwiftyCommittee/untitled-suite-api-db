d340998feb67da4616c29f9f5e297c75912db2a5
NJBLAGA <nathanblaga90@gmail.com>
2021-07-29 11:57:28 +1000

    fix routing bug

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,13 @@ Rails.application.routes.draw do
   root 'untitled#index'
 
   # Devise Generated routes for users, assigned to custom controller "registrations"
-   scope :api, defaults: { format: :json } do
+   
     devise_for :users,  controllers: {
       registrations: 'registrations',
     }
-  end
 
-  scope :api, defaults: { format: :json } do
     resources :lists
-  end
+
 end
 
 
