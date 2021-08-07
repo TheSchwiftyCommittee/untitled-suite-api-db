3bb44d97aab342f034149c3415bce4aa2095ac58
whywesmurfing <raymond.doan94@gmail.com>
2021-08-07 13:42:05 +1000

    Updated such that Profiles can only be accessed, and updated based on the JWT Token

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,12 +1,17 @@
 class ProfilesController < ApplicationController
-  before_action :authorized, only: [:auto_login]
+  # before_action :authorized, only: [:auto_login]
+  before_action :decoded_token, only: [:index, :update]
   before_action :set_profile, only: [:update]
 
   # GET /profiles
   def index
-      @user = User.find_by(email: params[:email])
+    @user = User.find_by(email: params[:email])
+    if decoded_token[0]["user_id"] == @user.id
       @profile_data = {profile: @user.profile, avatar: url_for(@user.profile.avatar)}
       render json: @profile_data
+    else
+      render json: {error: "You are not authorised to access this"}, status: :unauthorized
+    end
   end
 
   # POST /profiles
@@ -24,12 +29,16 @@ class ProfilesController < ApplicationController
   # PATCH/PUT/profiles/1
   def update
     @user = User.find_by(id: params[:id])
-    @profile = Profile.find_by(user_id: @user.id)
-    if @profile.update(profile_params)
-      @profile_data = {profile: @user.profile, avatar: url_for(@user.profile.avatar)}
-      render json: @profile_data
+    if decoded_token[0]["user_id"] == @user.id
+      @profile = Profile.find_by(user_id: @user.id)
+      if @profile.update(profile_params)
+        @profile_data = {profile: @user.profile, avatar: url_for(@user.profile.avatar)}
+        render json: @profile_data
+      else
+        render json: @profile.errors, status: :unprocessable_entity
+      end
     else
-      render json: @profile.errors, status: :unprocessable_entity
+      render json: {error: "You are not authorised to edit this"}, status: :unauthorized
     end
   end
 
